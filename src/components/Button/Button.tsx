3e26db4d59e7f559b1afbb29db8c77f6ab561ccb
mrimmoaee <mrimmoaee@gmail.com>
2022-09-12 03:45:27 +0600

    css added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 interface ButtonProps {
   label: string;
 }
 
+const ButtonStyle = styled.div`
+  background-color: red;
+  color: white;
+  padding: 10px;
+`;
+
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <ButtonStyle>{props.label}</ButtonStyle>;
 };
 
 export default Button;
